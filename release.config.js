97c9d354f978ec5718cb265d9ec84726e54b2026
Jayson Potter <iam@jaysonpotter.com>
2024-05-21 16:02:23 -0700

    feat(workflow): Updating the workflow to not update the changelog, but use the notes given in the releases.

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
             }
         }],
         '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
+        // '@semantic-release/changelog',
         // '@semantic-release/github',
     ]
 };
