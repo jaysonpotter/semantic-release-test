78008a63617b129bae8707f0bd288619f1c74a0b
Jayson Potter <iam@jaysonpotter.com>
2024-05-21 15:55:34 -0700

    feat(workflow): Trying out the wrokflow without automatically publishing the version tag, but generating it.

--- release.config.js
+++ release.config.js
@@ -3,21 +3,12 @@ module.exports = {
     plugins: [
         ['@semantic-release/commit-analyzer', {
             preset: 'conventionalcommits',
-            // releaseRules: [
-            //     { type: 'feat', release: 'minor' },
-            //     { type: 'fix', release: 'patch' },
-            //     { type: 'docs', release: 'patch' },
-            //     { type: 'style', release: 'patch' },
-            //     { type: 'refactor', release: 'patch' },
-            //     { type: 'perf', release: 'patch' },
-            //     { type: 'test', release: 'patch' },
-            // ],
             parserOpts: {
                 noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
             }
         }],
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
-        '@semantic-release/github',
+        // '@semantic-release/github',
     ]
 };
