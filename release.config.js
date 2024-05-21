72205cae1c92fb866f0348c0f0c5f3e38aa94f69
Jayson Potter <iam@jaysonpotter.com>
2024-05-21 11:00:32 -0700

    Setting up new project with semantic-release to test that out.

---
+++ release.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/github',
+        '@semantic-release/git',
+    ],
+    preset: 'angular',
+    releaseRules: [
+        { type: 'feat', release: 'minor' },
+        { type: 'fix', release: 'patch' },
+        { type: 'docs', release: 'patch' },
+        { type: 'style', release: 'patch' },
+        { type: 'refactor', release: 'patch' },
+        { type: 'perf', release: 'patch' },
+        { type: 'test', release: 'patch' },
+    ],
+    parserOpts: {
+        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+    },
+    writerOpts: {
+        commitsSort: ['subject', 'scope'],
+    },
+};
