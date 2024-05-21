172aab8fac03405babd0061541cc62587a5af10d
Jayson Potter <iam@jaysonpotter.com>
2024-05-21 15:35:13 -0700

    fix(workflow): Simplifying the config based on plugins documentation at https://semantic-release.gitbook.io/semantic-release/extending/plugins-list

--- release.config.js
+++ release.config.js
@@ -1,27 +1,39 @@
 module.exports = {
-    branches: [
-        { name: 'main', channel: 'release' }
-    ],
+    branches: ['main'],
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
-        '@semantic-release/github',
+        // ['@semantic-release/commit-analyzer', {
+        //     preset: 'conventionalcommits',
+        //     // releaseRules: [
+        //     //     { type: 'feat', release: 'minor' },
+        //     //     { type: 'fix', release: 'patch' },
+        //     //     { type: 'docs', release: 'patch' },
+        //     //     { type: 'style', release: 'patch' },
+        //     //     { type: 'refactor', release: 'patch' },
+        //     //     { type: 'perf', release: 'patch' },
+        //     //     { type: 'test', release: 'patch' },
+        //     // ],
+        //     parserOpts: {
+        //         noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        //     }
+        // }],
+        // '@semantic-release/release-notes-generator',
+        // '@semantic-release/changelog',
+        // '@semantic-release/github',
         // ['@semantic-release/git', {
         //     assets: ['CHANGELOG.md'],
         //     message: "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}"
         // }]
     ],
-    preset: 'angular',
-    releaseRules: [
-        { type: 'feat', release: 'minor' },
-        { type: 'fix', release: 'patch' },
-        { type: 'docs', release: 'patch' },
-        { type: 'style', release: 'patch' },
-        { type: 'refactor', release: 'patch' },
-        { type: 'perf', release: 'patch' },
-        { type: 'test', release: 'patch' },
-    ],
+    preset: 'conventionalcommits',
+    // releaseRules: [
+    //     { type: 'feat', release: 'minor' },
+    //     { type: 'fix', release: 'patch' },
+    //     { type: 'docs', release: 'patch' },
+    //     { type: 'style', release: 'patch' },
+    //     { type: 'refactor', release: 'patch' },
+    //     { type: 'perf', release: 'patch' },
+    //     { type: 'test', release: 'patch' },
+    // ],
     parserOpts: {
         noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
     },
