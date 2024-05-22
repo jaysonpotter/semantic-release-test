19e5c7819dd8a8827f6ec862f0a3434137694ffe
Jayson Potter <iam@jaysonpotter.com>
2024-05-22 14:16:49 -0700

    fix(workflow): Adding comments to help understand what the plugins are for.
    fix(config): Updated semantic-release/github to draft a release vs publishing automatically.
    
    feat(changelog): Removing based on redundancy.

--- release.config.js
+++ release.config.js
@@ -1,14 +1,23 @@
 module.exports = {
     branches: ['main'],
-    plugins: [
+    plugins : [
         ['@semantic-release/commit-analyzer', {
-            preset: 'conventionalcommits',
+            preset    : 'conventionalcommits',
             parserOpts: {
                 noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
             }
         }],
+
         '@semantic-release/release-notes-generator',
+        // @semantic-release/changelog can be added if we want to be able to have the semantic-release-bot automatically
+        // commit those changes to the CHANGELOG file to main. However, this is redundant to the notes in our releases.
+        // Would also need to generate a "principle admin" account that is able to bypass branch protections. Generating
+        // a PR with those changes like release-please handles it does not appear to be an option.
         // '@semantic-release/changelog',
-        '@semantic-release/github',
+
+        // @semantic-release/github will automatically publish the tagged release, remove to make that a manual step.
+        ['@semantic-release/github', {
+            draftRelease: true
+        }],
     ]
 };
